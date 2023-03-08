f4c205b3ec39dcf6dd7870f7c2934494083b560f
Naman Jain <namanviber@gmail.com>
2023-03-08 19:16:53 +0530

    8 march commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
-import 'package:project2/screens/recommender_screen.dart';
-import 'package:project2/screens/settings_screen.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:project2/screens/watchlist_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,11 +12,22 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   String user = "Naman";
   int screen_index = 0;
+  final List<String> imgLists = [
+    'assets/images/testimg1.png',
+    'assets/images/testimg3.png',
+    'assets/images/testimg4.png',
+    'assets/images/testimg5.png',
+    'assets/images/testimg6.png',
+    'assets/images/testimg7.png',
+  ];
+  final CarouselController carouselController = CarouselController();
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         toolbarHeight: 100,
         backgroundColor: const Color(0xFF09090F),
         title: Row(
@@ -39,149 +49,267 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-                decoration: InputDecoration(
-                    hintText: "Search for movies, TV shows etc...",
-                    filled: true,
-                    fillColor: const Color(0xFF51535D),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                        borderSide: BorderSide.none),
-                    prefixIcon: const Icon(Icons.search),
-                    prefixIconColor: Colors.white)),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              alignment: Alignment.topLeft,
-              child: const Text("Filters",
-                  style: TextStyle(
-                      fontFamily: "Inter",
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16)),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                InkWell(
-                  child: Container(
-                    height: 55.0,
-                    width: 55.0,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color: Color(0xff51535E),
-                    ),
-                    child: const Icon(Icons.grid_view_rounded,
-                        color: Colors.white, size: 40.0),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                  decoration: InputDecoration(
+                      hintText: "Search for movies, TV shows etc...",
+                      filled: true,
+                      fillColor: const Color(0xFF51535D),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none),
+                      prefixIcon: const Icon(Icons.search),
+                      suffixIcon: const Icon(Icons.mic),
+                      suffixIconColor: Colors.white,
+                      prefixIconColor: Colors.white),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: const Text("Quick Access Panel",
+                    style: TextStyle(
+                        fontFamily: "Inter",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: [
+                      InkWell(
+                        child: Container(
+                          height: 55.0,
+                          width: 55.0,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: Color(0xff51535E),
+                          ),
+                          child: const Icon(Icons.grid_view_rounded,
+                              color: Colors.white, size: 40.0),
+                        ),
+                        onTap: () {},
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text("Grid")
+                    ],
+                  ),Column(
+                    children: [
+                      InkWell(
+                        child: Container(
+                          height: 55.0,
+                          width: 55.0,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: Color(0xff51535E),
+                          ),
+                          child: const Icon(Icons.star,
+                              color: Colors.white, size: 40.0),
+                        ),
+                        onTap: () {},
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text("Favourites")
+                    ],
+                  ),Column(
+                    children: [
+                      InkWell(
+                        child: Container(
+                          height: 55.0,
+                          width: 55.0,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: Color(0xff51535E),
+                          ),
+                          child: const Icon(Icons.language,
+                              color: Colors.white, size: 40.0),
+                        ),
+                        onTap: () {},
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text("Language")
+                    ],
+                  ),Column(
+                    children: [
+                      InkWell(
+                        child: Container(
+                          height: 55.0,
+                          width: 55.0,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: Color(0xff51535E),
+                          ),
+                          child: const Icon(Icons.history_rounded,
+                              color: Colors.white, size: 40.0),
+                        ),
+                        onTap: () {},
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text("Watched")
+                    ],
                   ),
-                  onTap: () {},
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: const Text("Now Showing",
+                    style: TextStyle(
+                        fontFamily: "Inter",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              CarouselSlider(
+                items: imgLists
+                    .map((item) => Container(
+                          child: Center(
+                            child: Image.asset(
+                              item,
+                              width: 200,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ))
+                    .toList(),
+                options: CarouselOptions(
+                  height: 270,
+                  autoPlay: true,
+                  aspectRatio: 2.0,
+                  enlargeCenterPage: true,
                 ),
-                InkWell(
-                  child: Container(
-                    height: 55.0,
-                    width: 55.0,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color: Color(0xff51535E),
-                    ),
-                    child: const Icon(Icons.grid_view_rounded,
-                        color: Colors.white, size: 40.0),
-                  ),
-                  onTap: () {},
-                ),
-                InkWell(
-                  child: Container(
-                    height: 55.0,
-                    width: 55.0,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color: Color(0xff51535E),
-                    ),
-                    child: const Icon(Icons.grid_view_rounded,
-                        color: Colors.white, size: 40.0),
-                  ),
-                  onTap: () {},
-                ),
-                InkWell(
-                  child: Container(
-                    height: 55.0,
-                    width: 55.0,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color: Color(0xff51535E),
-                    ),
-                    child: const Icon(Icons.grid_view_rounded,
-                        color: Colors.white, size: 40.0),
-                  ),
-                  onTap: () {},
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              alignment: Alignment.topLeft,
-              child: const Text("Now Showing",
-                  style: TextStyle(
-                      fontFamily: "Inter",
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20)),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const SizedBox(
-              height: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              alignment: Alignment.topLeft,
-              child: const Text("Movies for You",
-                  style: TextStyle(
-                      fontFamily: "Inter",
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20)),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: ListView.builder(
-                  itemCount: 10,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      leading: Icon(Icons.star),
-                      trailing: Icon(Icons.library_add),
-                      title: Text("Bhuj $index"),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: const Text("Movies for You",
+                    style: TextStyle(
+                        fontFamily: "Inter",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 250,
+                width: double.maxFinite,
+                child: ListView.builder(
+                  itemCount: imgLists.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, index) {
+                    return Container(
+                      height: 170,
+                      width: 160,
+                      margin: EdgeInsets.symmetric(horizontal: 8),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(imgLists[index]),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
                     );
-                  }),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              alignment: Alignment.topLeft,
-              child: const Text("Movies for You",
-                  style: TextStyle(
-                      fontFamily: "Inter",
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20)),
-            ),
-          ],
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: const Text("Trending",
+                    style: TextStyle(
+                        fontFamily: "Inter",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 250,
+                width: double.maxFinite,
+                child: ListView.builder(
+                  itemCount: imgLists.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, index) {
+                    return Container(
+                      height: 170,
+                      width: 160,
+                      margin: EdgeInsets.symmetric(horizontal: 8),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(imgLists[index]),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: const Text("Top 10 in India",
+                    style: TextStyle(
+                        fontFamily: "Inter",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 250,
+                width: double.maxFinite,
+                child: ListView.builder(
+                  itemCount: imgLists.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, index) {
+                    return Container(
+                      height: 170,
+                      width: 160,
+                      margin: EdgeInsets.symmetric(horizontal: 8),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(imgLists[index]),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -189,6 +317,12 @@ class _HomeScreenState extends State<HomeScreen> {
         onTap: (index) {
           setState(() {
             screen_index = index;
+            if (screen_index == 2){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => WatchlistScreen()));
+            }
+            else if (screen_index == 0){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+            }
           });
         },
         currentIndex: screen_index,
--- lib/screens/watchlist_screen.dart
+++ lib/screens/watchlist_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
 class WatchlistScreen extends StatefulWidget {
   const WatchlistScreen({Key? key}) : super(key: key);
@@ -8,30 +9,177 @@ class WatchlistScreen extends StatefulWidget {
 }
 
 class _WatchlistScreenState extends State<WatchlistScreen> {
+  int screen_index = 2;
   @override
-  int screen_index = 0;
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Container(
-          child: Text("This is Watchlist Screen"),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        toolbarHeight: 100,
+        backgroundColor: const Color(0xFF09090F),
+        title: TextField(
+          decoration: InputDecoration(
+              hintText: "Search for movies, TV shows etc...",
+              filled: true,
+              fillColor: const Color(0xFF51535D),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide.none),
+              prefixIcon: const Icon(Icons.search),
+              suffixIcon: const Icon(Icons.history_rounded),
+              suffixIconColor: Colors.white,
+              prefixIconColor: Colors.white),
         ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      InkWell(
+                        child: Text("Movies",
+                            style: TextStyle(
+                                fontFamily: "Inter",
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                decoration: TextDecoration.underline,
+                                decorationColor: Color(0xFF00B5F3),
+                                decorationThickness: 2.0)),
+                        onTap: () {},
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      InkWell(
+                        child: Text(
+                          "Shows",
+                          style: TextStyle(
+                              fontFamily: "Inter",
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.search),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Icon(Icons.filter_list_outlined),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Icon(Icons.grid_view),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 30,
+                      width: 150,
+                      alignment: Alignment.center,
+                      child: Text(
+                        "All Movies",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Inter",
+                            fontSize: 12),
+                        textAlign: TextAlign.center,
+                      ),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF00B5F3),
+                        borderRadius: BorderRadius.circular(6)
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),Container(
+                      height: 30,
+                      width: 150,
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Unwatched Movies",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Inter",
+                            fontSize: 12),
+                        textAlign: TextAlign.center,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(6)
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),Container(
+                      height: 30,
+                      width: 150,
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Watched Movies",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Inter",
+                            fontSize: 12),
+                        textAlign: TextAlign.center,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(6)
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         onTap: (index) {
           setState(() {
             screen_index = index;
+            if (screen_index == 2) {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => WatchlistScreen()));
+            } else if (screen_index == 0) {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()));
+            }
           });
         },
         currentIndex: screen_index,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.play_circle), label: "Recommender"),
-          BottomNavigationBarItem(icon: Icon(Icons.library_add), label: "Watchlist"),
-          BottomNavigationBarItem(icon: Icon(Icons.settings_rounded), label: "Settings"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.play_circle), label: "Recommender"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.library_add), label: "Watchlist"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings_rounded), label: "Settings"),
         ],
         backgroundColor: const Color(0xFF09090F),
-        selectedItemColor: Color(0xFF00B5F3),
+        selectedItemColor: const Color(0xFF00B5F3),
         unselectedItemColor: Colors.white,
         // type: BottomNavigationBarType.shifting,
       ),
